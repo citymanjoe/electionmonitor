2edf1ad52b5d7fafc3ee042bdc4cbf2a805deffc
citymanjoe <einjoku@gmail.com>
2022-10-05 21:49:06 +0100

    Initial commit

---
+++ src/main/java/com/ngelection/electionmonitor/ElectionmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ngelection.electionmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionmonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngelection/electionmonitor/ElectionmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngelection.electionmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
